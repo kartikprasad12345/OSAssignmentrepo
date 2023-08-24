5d7f9dc30843a55fba6ec372a59374b62c2fdfaf
kartikprasad12345 <119179751+kartikprasad12345@users.noreply.github.com>
2023-08-24 20:04:00 +0530

    Update loader.c

--- upload (1)/starter/without-bonus/loader.c
+++ upload (1)/starter/without-bonus/loader.c
@@ -7,31 +7,80 @@ int fd;
 /*
  * release memory and other cleanups
  */
-void loader_cleanup() 
-{
+void loader_cleanup() {
+    if(ehdr!=NULL){
+        free(ehdr);
+    }
+    if(phdr!=NULL){
+        free(phdr);
+    }
   
 }
+void *traversingphdr(int cur,int max,Elf32_Phdr *phdr){
+    if(cur==max-1){
+        return NULL;
+    }
+    if (phdr[cur].p_type == PT_LOAD){
+        if((phdr[cur].p_vaddr<=ehdr->e_entry)&&(ehdr->e_entry<phdr[cur].p_vaddr+phdr[cur].p_memsz)){
+            return mmap((void*)phdr[cur].p_vaddr,phdr[cur].p_memsz,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE|MAP_FIXED,fd,phdr[cur].p_offset);
+        }
 
+    }
+    return traversingphdr(cur+1,max,phdr);
+}
+int checkelf(Elf32_Ehdr *ehdr){
+    printf("bro");
+}
 /*
  * Load and run the ELF executable file
  */
-void load_and_run_elf(char** exe) {
-  fd = open(exe[1], O_RDONLY);
-  ehdr = (Elffd ; // the ehdr is at 0 offset . So the file pointer should also be pointing at it . 
-  unsigned int entrypoint_address = ehdr ;
-  // 1. Load entire binary content into the memory from the ELF file.
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
-  // 3. Allocate memory of the size "p_memsz" using mmap function 
-  //    and then copy the segment content
-  // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
-  // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
-  
+void load_and_run_elf(char **exe)
+{
+    
+    fd = open(exe[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Not being able to open ELF file");
+        return;
+    }
 
-  // 6. Call the "_start" method and print the value returned from the "_start"
-  int result = _start();
-  printf("User _start return value = %d\n",result);
+    // Load ELF header
+    ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
+    if (!ehdr) {
+        printf("Could not allocate\n");
+        return;
+    }
+
+    int fileread = read(fd, ehdr, sizeof(Elf32_Ehdr));
+    if(fileread==-1){
+        printf("Could not read file\n");
+    }
+    if(ehdr->e_ident[0]==0x7f){
+        printf("!\n");
+    }
+    // Load program headers
+    phdr = (Elf32_Phdr *)malloc(ehdr->e_phnum * sizeof(Elf32_Phdr));
+
+    // Seek to the program headers' offset in the file
+    lseek(fd, ehdr->e_phoff, SEEK_SET);
+    int fileread2=read(fd, phdr, ehdr->e_phnum * sizeof(Elf32_Phdr));
+    if(fileread2==-1){
+        printf("Coulld not read file\n");
+    }
+    // Find and load the executable segment
+    int a=0;
+    int lenph=ehdr->e_phnum;
+    void *virtual_mem=traversingphdr(0,lenph,phdr);
+
+    unsigned int temp;
+    if(virtual_mem==NULL){
+        printf("No entry point found\n");
+    }
+    int (*_start)(void)=(void*)ehdr->e_entry;
+    int result = _start();
+    printf("User _start return value = %d\n",result);
+    
 }
+
 
 int main(int argc, char** argv) 
 {
@@ -41,7 +90,7 @@ int main(int argc, char** argv)
   }
   // 1. carry out necessary checks on the input ELF file
   // 2. passing it to the loader for carrying out the loading/execution
-  load_and_run_elf(argv[1]);
+  load_and_run_elf(argv);
   // 3. invoke the cleanup routine inside the loader  
   loader_cleanup();
   return 0;
